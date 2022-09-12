886f2475a70c9c2663beb56d77a5549484cdfe7f
malken21 <Malken_Marumasa@outlook.jp>
2022-09-12 12:00:53 +0900

    Add files via upload

---
+++ src/main/java/marumasa/dispenser_falling_block/Config.java
@@ -0,0 +1,18 @@
+package marumasa.dispenser_falling_block;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class Config {
+    public final String[] materials;
+    public final boolean Log;
+    public final boolean Consume;
+
+    public Config(minecraft plugin) {
+        plugin.saveDefaultConfig();
+        FileConfiguration config = plugin.getConfig();
+
+        materials = config.getStringList("BlockList").toArray(new String[0]);
+        Log = config.getBoolean("Log");
+        Consume = config.getBoolean("Consume");
+    }
+}
---
+++ src/main/java/marumasa/dispenser_falling_block/eventListener.java
@@ -0,0 +1,54 @@
+package marumasa.dispenser_falling_block;
+
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.block.Dispenser;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class eventListener implements Listener {
+
+    private final Config con;
+    private final minecraft mc;
+
+    public eventListener(Config config, minecraft minecraft) {
+        con = config;
+        mc = minecraft;
+    }
+
+    @EventHandler
+    public void onEntity(BlockDispenseEvent event) {
+        final ItemStack item = event.getItem();
+        final Material itemM = item.getType();
+        final String itemMS = itemM.toString();
+
+        if (con.Log) Bukkit.getLogger().info(itemMS);
+
+        for (String ConfigM : con.materials) {
+            if (!ConfigM.equalsIgnoreCase(itemMS)) continue;
+            //-----start!!-----//
+            final Block block = event.getBlock();
+
+            final Location loc = block.getLocation().add(0.5, 0, 0.5);
+            final String facing = block.getBlockData().getAsString().split("[=,]")[1];
+
+            switch (facing) {
+                case "east" -> loc.add(1, 0, 0);
+                case "west" -> loc.add(-1, 0, 0);
+                case "up" -> loc.add(0, 1, 0);
+                case "down" -> loc.add(0, -1, 0);
+                case "south" -> loc.add(0, 0, 1);
+                case "north" -> loc.add(0, 0, -1);
+            }
+            final World world = block.getWorld();
+
+            final Dispenser dispenser = (Dispenser) event.getBlock().getState();
+            if (con.Consume) new itemRemove(dispenser.getInventory(), itemM).runTaskTimer(mc, 0L, 0L);
+            world.spawnFallingBlock(loc, itemM.createBlockData());
+            event.setCancelled(true);
+            break;
+        }
+    }
+}
